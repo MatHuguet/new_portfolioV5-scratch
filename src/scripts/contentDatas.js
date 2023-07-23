66cd4dd6ae07b77033adc60ca2f39b90b482c7c0
MathieuH <56202917+MatHuguet@users.noreply.github.com>
2023-07-23 16:00:14 +0200

    add js data file and import to index.js to display data in content section

---
+++ src/scripts/contentDatas.js
@@ -0,0 +1,43 @@
+export const contentData = [
+    {
+    title: "about",
+    content: `<h2>A propos de moi</h2>
+            <br></br>
+            <p>Je suis Mathieu, développeur junior. Après plus d'une dizaine d'année dans l'hôtellerie, je me suis reconverti dans le développement web et web mobile depuis peu.</p>
+            <br></br>
+            <p>Passioné d'informatique depuis mon plus jeune âge, je souhaite mettre à profit mes compétences fraîchement acquises, ma curiosité et ma forte créativité afin de développer des applications visuellement attrayantes et ergonomiques pour les utilisateursq.</p>
+    `
+    },
+    {
+    title: "cv",
+    content: `<h2>Expériences professionelles</h2>
+            <br></br>
+            <div class="job-section-container">
+                <div class="exp-date">
+                    <p>Du 08/2022 à aujourd'hui</p>
+                    <br></br>
+
+                </div
+                <div class="job-title>
+                    <h3 class="cv-pro-title">Régisseur</h3>"
+                </div>
+                <div class="job-description">
+                    <p>M'occuper d'un domaine</p>
+                </div>
+            </div>
+            <br></br>
+    `
+    },
+    {
+    title: "skills",
+    content: `<h2>Hello skills</h2>`
+    },
+    {
+    title: "portfolio",
+    content: `<h2>Hello portfolio</h2>`
+    },
+    {
+    title: "contact",
+    content: `<h2>Hello contact</h2>`
+    }
+]
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,19 @@
+import { contentData } from "./contentDatas.js";
+console.log(contentData[0].content);
+
 const navItem = document.getElementsByClassName('nav-item');
 const sideTitle = document.getElementById('side-title');
+const content = document.getElementById('section-content')
+
+
+const apropos = contentData[0].content;
+const cv = contentData[1].content;
+const competences = contentData[2].content;
+const portfolio = contentData[3].content;
+const contact = contentData[4].content;
 
 sideTitle.innerText = 'A PROPOS';
+content.innerHTML = apropos;
 
 
      
@@ -11,9 +23,34 @@ attr.forEach(element => {
         sideTitle.innerText = element.innerText;
         document.querySelector('.active').classList.remove('active');
         element.classList.add('active');
+        addContent();
     })
 
 });
+
+
+
+function addContent() {
+
+    switch(sideTitle.innerText) {
+        case 'A PROPOS':
+            content.innerHTML = apropos;
+            break;
+        case 'CV':
+            content.innerHTML = cv;
+            break;
+        case 'COMPETENCES':
+            content.innerHTML = competences;
+            break;
+        case 'PORTFOLIO':
+            content.innerHTML = portfolio;
+            break;
+        case 'CONTACT':
+            content.innerHTML = contact;
+            break;
+    }
+}
+
 
 
 
