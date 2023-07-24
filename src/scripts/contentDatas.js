73bd00d8b886842cc78a89c714970c48c2a0dc34
MathieuH <56202917+MatHuguet@users.noreply.github.com>
2023-07-24 18:42:33 +0200

    fixed and cleaned css to responsive on every devices. TODO inner content styling for fonts

--- src/scripts/contentDatas.js
+++ src/scripts/contentDatas.js
@@ -2,9 +2,7 @@ export const contentData = [
     {
     title: "about",
     content: `<h2 class="content-head">A propos de moi</h2>
-            <br></br>
             <p>Je suis Mathieu, développeur junior. Après plus d'une dizaine d'année dans l'hôtellerie, je me suis reconverti dans le développement web et web mobile depuis peu.</p>
-            <br></br>
             <p>Passioné d'informatique depuis mon plus jeune âge, je souhaite mettre à profit mes compétences fraîchement acquises, ma curiosité et ma forte créativité afin de développer des applications visuellement attrayantes et ergonomiques pour les utilisateursq.</p>
     `
     },
